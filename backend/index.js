8dcdc0b57f577b360bd094491a4252f5089393b8
Nikhil sabban <141856766+nikhilsabban@users.noreply.github.com>
2025-05-15 04:13:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const userRoute = require("./routes/user");
 const app = express();
 const PORT = 8001;
 
-connectToMongoDB(process.env.MONGODB ?? "mongodb+srv://nikhilsabban:W380PyUVpghxzftg@cluster0.izd8vlo.mongodb.net/").then(() =>
+connectToMongoDB(process.env.MONGODB ?? "mongodb+srv://nikhilsabban:W380PyUVpghxzftg@cluster0.izd8vlo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() =>
   console.log("Mongodb connected")
 );
 
